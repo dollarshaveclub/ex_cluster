2c143aca78d6c2d00641389410de790185ba5fe1
Chirag Singh Toor <chiragtoor@gmail.com>
2018-12-07 14:49:36 -0800

    Initial commit

---
+++ lib/ex_cluster.ex
@@ -0,0 +1,18 @@
+defmodule ExCluster do
+  @moduledoc """
+  Documentation for ExCluster.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCluster.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_cluster_test.exs
@@ -0,0 +1,8 @@
+defmodule ExClusterTest do
+  use ExUnit.Case
+  doctest ExCluster
+
+  test "greets the world" do
+    assert ExCluster.hello() == :world
+  end
+end
